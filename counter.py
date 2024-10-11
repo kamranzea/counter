b6ea4d2edc55286af58c9e4df44cf85733b23dda
Kamran Zia <156909711+kamranzea@users.noreply.github.com>
2024-10-11 04:52:54 -0700

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,19 +1,17 @@
+import argparse
 import time
 import keyboard
 
-count = int(input("Enter any integer : "))
-#inputint = int(inputint)
-
-def counter (count):
-
-    i = 0
-    while i < count:
-        
-        time.sleep(1)
-        i = i + 1
-        print(i)
-        
+def counter(x):
+    for i in range(x):
         if keyboard.is_pressed(' '):
             break
+        print(i)
+        time.sleep(1)
 
-counter(count)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--target', type=int, required=True)
+    args = parser.parse_args()
+    
+    counter(args.target)
